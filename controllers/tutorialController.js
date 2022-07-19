8bef7dfa3c114f850f5748000fbce62414b61e8b
grahagandangr <grahagandangr@gmail.com>
2022-07-19 21:19:47 +0700

    add get java and golang tutorial feature

--- controllers/tutorialController.js
+++ controllers/tutorialController.js
@@ -33,6 +33,66 @@ class TutorialController {
       next(err);
     }
   }
+
+  static async getGolangTutorial(req, res, next) {
+    try {
+      const { data } = await axios.get(youtubeSearchApiUrl, {
+        params: {
+          part: "snippet",
+          q: "golang tutorial",
+          type: "video",
+          order: "viewCount",
+          maxResults: "20",
+          key: youtubeApiKey,
+        },
+      });
+      const phpTutorials = data.items.map((item) => {
+        return {
+          title: item.snippet.title,
+          thumbnail: item.snippet.thumbnails.high.url,
+          channelTitle: item.snippet.channelTitle,
+          channelUrl: `${youtubeBaseUrl}/channel/${item.snippet.channelId}`,
+          description: item.snippet.description,
+          publishedAt: item.snippet.publishedAt,
+          videoUrl: `${youtubeBaseUrl}/watch?v=${item.id.videoId}`,
+        };
+      });
+
+      res.status(200).json(phpTutorials);
+    } catch (err) {
+      next(err);
+    }
+  }
+
+  static async getJavaTutorial(req, res, next) {
+    try {
+      const { data } = await axios.get(youtubeSearchApiUrl, {
+        params: {
+          part: "snippet",
+          q: "java tutorial",
+          type: "video",
+          order: "viewCount",
+          maxResults: "20",
+          key: youtubeApiKey,
+        },
+      });
+      const phpTutorials = data.items.map((item) => {
+        return {
+          title: item.snippet.title,
+          thumbnail: item.snippet.thumbnails.high.url,
+          channelTitle: item.snippet.channelTitle,
+          channelUrl: `${youtubeBaseUrl}/channel/${item.snippet.channelId}`,
+          description: item.snippet.description,
+          publishedAt: item.snippet.publishedAt,
+          videoUrl: `${youtubeBaseUrl}/watch?v=${item.id.videoId}`,
+        };
+      });
+
+      res.status(200).json(phpTutorials);
+    } catch (err) {
+      next(err);
+    }
+  }
 }
 
 module.exports = TutorialController;
--- routes/tutorials.js
+++ routes/tutorials.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const TutorialController = require("../controllers/tutorialController");
 
 router.get("/nodejs", TutorialController.getNodejsTutorial);
-router.get("/php", TutorialController);
-router.get("/golang", TutorialController);
+router.get("/java", TutorialController.getJavaTutorial);
+router.get("/golang", TutorialController.getGolangTutorial);
 
 module.exports = router;
