89ad336c457e3888d458962fa66b64d8b89be922
unknown <jonathanadrino@gmail.com>
2022-07-19 16:49:47 +0800

    seed user

---
+++ seeders/20220719084332-seed-user.js
@@ -0,0 +1,41 @@
+"use strict";
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+    const bcrypt = require("bcryptjs");
+    await queryInterface.bulkInsert(
+      "Users",
+      [
+        {
+          name: "admin",
+          email: "admin1@admin.com",
+          password: bcrypt.hashSync("12345"),
+          address: "test",
+          phoneNumber: "085208520852",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    );
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete("Users", null, {});
+  },
+};
