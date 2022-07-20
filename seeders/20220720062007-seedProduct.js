822ed762a87cb02f9ed770102d36edd1e55d808e
noruhm <henryhrty@gmail.com>
2022-07-20 23:55:30 +0700

    real fix seeder

--- seeders/20220720062007-seedProduct.js
+++ seeders/20220720062007-seedProduct.js
@@ -3,6 +3,10 @@
 module.exports = {
   async up(queryInterface, Sequelize) {
     const dummy = require("../dummy.json");
+    dummy.forEach((el) => {
+      el.createdAt = new Date();
+      el.updatedAt = new Date();
+    });
     await queryInterface.bulkInsert("Products", dummy);
   },
 
