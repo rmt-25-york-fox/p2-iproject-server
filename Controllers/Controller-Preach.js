c428e37ac02070359a958434ee33ff54177d1f08
alirezaG0104 <alireza0104@gmail.com>
2022-07-20 23:50:49 +0700

    feature completed

--- Controllers/Controller-Preach.js
+++ Controllers/Controller-Preach.js
@@ -4,8 +4,12 @@ class Controller{
     static async addPreach(req,res,next){
         try {
             const { title, date, VideoUrl} = req.body
+           let convertedDate = Date.parse(date)
 
-            const newPreach = await Preach.create({ title, date: date , VideoUrl, PastorId: req.user.id})
+            const newPreach = await Preach.create({ title, date: convertedDate , VideoUrl, PastorId: req.user.id})
+            if(!newPreach){
+                throw err
+            }
             res.status(201).json(newPreach)
         } catch (err) {
             next(err)
