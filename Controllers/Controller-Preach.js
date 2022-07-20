558bdaf0c99a203598af957df1b0609f29f3b38c
alirezaG0104 <alireza0104@gmail.com>
2022-07-20 22:26:31 +0700

    empty

--- Controllers/Controller-Preach.js
+++ Controllers/Controller-Preach.js
@@ -4,7 +4,7 @@ class Controller{
     static async addPreach(req,res,next){
         try {
             const { title, date, VideoUrl} = req.body
-            date =  date +" 04:00:00"
+
             const newPreach = await Preach.create({ title, date: date , VideoUrl, PastorId: req.user.id})
             res.status(201).json(newPreach)
         } catch (err) {
