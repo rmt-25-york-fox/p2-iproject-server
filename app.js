9d3ab0edeb02cb9a666a333215c8665cead884c7
noruhm <henryhrty@gmail.com>
2022-07-20 14:25:52 +0700

    testdeply

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ const express = require("express");
 const app = express();
 const router = require("./router/index");
 const errorHandler = require("./middlewares/errorHandlers");
-const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
@@ -16,6 +15,4 @@ app.use(express.json());
 app.use(router);
 app.use(errorHandler);
 
-app.listen(port, () => {
-  console.log(`APP RUNNING IN ${port}`);
-});
+module.exports = app;
