55dfd7ab5f992b9ab5082397f37bcb0290d5808c
AndrianFauzi <andrianfauzi1995@gmail.com>
2022-07-21 09:54:57 +0700

    final

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -1,8 +1,8 @@
 const { User, Product, Category } = require("../models/index");
 const { bcrypt, jwt, getToken } = require("../helper/index");
-
+const nodemailer = require("nodemailer");
 class Controller {
-  static async Register(req, res, next) {
+  static async Register(req, res) {
     try {
       const { username, email, password, phoneNumber, address } = req.body;
       const newUser = await User.create({
@@ -16,7 +16,7 @@ class Controller {
         message: `User dengan id ${newUser.id} successfully created`,
       });
     } catch (error) {
-      next(error);
+      res.status(409).json({ message: "Fail to register" });
     }
   }
   static async Login(req, res, next) {
@@ -47,7 +47,13 @@ class Controller {
         username: foundUser.username,
       });
     } catch (error) {
-      next(error);
+      if (error.name === "error invalid username or email or password") {
+        res
+          .status(404)
+          .json({ message: "error invalid username or email or password" });
+      } else {
+        res.status(500).json({ message: "ISE" });
+      }
     }
   }
   static async getAll(req, res, next) {
@@ -55,7 +61,7 @@ class Controller {
       const products = await Product.findAll();
       res.status(200).json(products);
     } catch (error) {
-      next(error);
+      res.status(500).json({ message: "ISE" });
     }
   }
   static async selectDetailProduct(req, res, next) {
@@ -68,8 +74,44 @@ class Controller {
       }
       res.status(200).json(product);
     } catch (error) {
-      next(error);
+      if (error.name === "not found detail") {
+        res.status(404).json({ message: "Not found product" });
+      } else {
+        res.status(500).json({ message: "ISE" });
+      }
     }
+  }
+  static getOtp(req, res, next) {
+    const { email } = req.body;
+    const otp1 = Math.floor(Math.random() * 8);
+    const otp2 = Math.floor(Math.random() * 8);
+    const otp3 = Math.floor(Math.random() * 8);
+    const otp4 = Math.floor(Math.random() * 8);
+    const otp = `${otp1}${otp2}${otp3}${otp4}`;
+    let mailTransporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: "andrianfauzi1995@gmail.com",
+        pass: "lehwmeyvlqyoaacm",
+      },
+    });
+
+    let mailDetails = {
+      from: "andrianfauzi1995@gmail.com",
+      to: email,
+      subject: "Test mail",
+      text: `Hello customer your otp is ${otp}`,
+    };
+
+    mailTransporter.sendMail(mailDetails, function (err, data) {
+      if (err) {
+        res.status(err).json(err);
+        console.log("Error Occurs");
+      } else {
+        res.status(201).json(otp);
+        console.log("Email sent successfully");
+      }
+    });
   }
 }
 
--- index.js
+++ index.js
@@ -3,8 +3,7 @@ const express = require("express");
 const app = express();
 var cors = require("cors");
 const Controller = require("./controllers/controllers");
-const errorHandler = require("./middlewares/errorHandler");
-const port = 10000;
+const port = 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(express.json());
@@ -12,7 +11,7 @@ app.post("/register", Controller.Register);
 app.post("/login", Controller.Login);
 app.get("/listProducts", Controller.getAll);
 app.get("/listProducts/:id", Controller.selectDetailProduct);
-app.use(errorHandler);
+app.post("/getOtp", Controller.getOtp);
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
