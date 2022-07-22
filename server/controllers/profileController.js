e3f236fb5955adea0c88cffa3accceb4b6e09dd7
menciusm <mencius999@gmail.com>
2022-07-22 11:02:31 +0700

    feat: multer & imageKit API

--- server/controllers/profileController.js
+++ server/controllers/profileController.js
@@ -1,9 +1,41 @@
 "use strict";
 
+const ImageKit = require("imagekit");
+const { Photo } = require("../models");
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
+const imagekit = new ImageKit({
+  publicKey: "public_Xp0PrQ/UvqKtBfSvrr58PDlOrvE=",
+  privateKey: PRIVATE_KEY,
+  urlEndpoint: "https://ik.imagekit.io/menmencius",
+});
+
 class ProfileController {
   static async uploadImage(req, res, next) {
     try {
-      console.log(req.file, "file");
+      const { id } = req.user;
+      let image;
+
+      if (req.file) {
+        image = req.file.buffer.toString("base64");
+      }
+
+      imagekit
+        .upload({
+          file: image, //required
+          fileName: req.file.originalname, //required
+        })
+        .then((response) => {})
+        .catch((error) => {
+          console.log(error);
+        });
+
+      const response = await Photo.create({
+        UserId: id,
+        imageUrl: image,
+      });
+
+      res.status(201).json(response);
     } catch (error) {
       next(error);
     }
--- server/routes/index.js
+++ server/routes/index.js
@@ -2,18 +2,20 @@ const routes = require("express").Router();
 const UserController = require("../controllers/userController");
 const errorHandler = require("../middlewares/errorhandler");
 const authentication = require("../middlewares/authentication");
+const multer = require("multer");
+const upload = multer({ storage: multer.memoryStorage() });
 const ProfileController = require("../controllers/profileController");
-const upload = require("../helpers/multer");
 
 routes.post("/register", UserController.register);
 routes.post("/login", UserController.login);
 
+routes.use(authentication);
+
 routes.post(
   "/profile/upload",
-  upload.single("files"),
+  upload.single("file"),
   ProfileController.uploadImage
 );
-routes.use(authentication);
 
 routes.use(errorHandler);
 
