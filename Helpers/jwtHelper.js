5b4e63bfbc9dba6e59c0ad39310ea24fd39aa5af
alirezaG0104 <alireza0104@gmail.com>
2022-07-22 08:26:32 +0700

    .env added

--- Helpers/jwtHelper.js
+++ Helpers/jwtHelper.js
@@ -1,13 +1,13 @@
 const jwt = require('jsonwebtoken')
 
 const createToken = (payload) =>{
-    return jwt.sign(payload, "key-ngasal", {
+    return jwt.sign(payload, process.env.SECRET_KEY, {
         expiresIn : "1h"
     })
 }
 
 const verifyToken = (token) =>{
-    return jwt.verify(token, "key-ngasal")
+    return jwt.verify(token, process.env.SECRET_KEY)
 }
 
 module.exports = {createToken, verifyToken}
